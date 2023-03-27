62a59d6d4a27ac7533d19177970fc6a760f4d34f
shevapav <pabut@LAPTOP-D0CL22KB>
2023-03-27 10:20:15 +0200

    Implemented fully tail utility

--- tail.c
+++ tail.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_LINE_LENGTH 4095
 
@@ -9,10 +10,11 @@ typedef struct{
     int start_index;
     int end_index;
     int length;
+    bool is_full;
 }circular_buffer;
 
 circular_buffer* cb_create(int n){
-    circular_buffer* cb = malloc(sieof(circular_buffer));
+    circular_buffer* cb = malloc(sizeof(circular_buffer));
     if(cb == NULL){
         fprintf(stderr, "Failed to allocate memory\n");
         return NULL;
@@ -20,6 +22,7 @@ circular_buffer* cb_create(int n){
     cb->length = n;
     cb->start_index = 0;
     cb->end_index = 0;
+    cb->is_full = false;
     if((cb->array = malloc(n * sizeof(char*))) == NULL){
         fprintf(stderr, "Failed to allocate memory\n");
         free(cb);
@@ -36,11 +39,18 @@ circular_buffer* cb_create(int n){
     return cb;
 }
 
-
 void cb_put(circular_buffer* cb, char* line){
-    strcpy(cb->array[cb->end_index], line);
-    cb->end_index = ++cb->end_index % cb->length;
+    if(cb->is_full){
+        cb->start_index++;
+        cb->start_index %= cb->length;
+    }
+    strcpy(cb->array[cb->end_index++], line);
+    if(cb->end_index >= cb->length){
+        cb->end_index %= cb->length;
+        cb->is_full = true;
+    }
 }
+
 
 
 char* cb_get(circular_buffer* cb){
@@ -57,13 +67,19 @@ void cb_free(circular_buffer* cb){
     free(cb);
 }
 
+void close_file_stream(FILE* file){
+    if(file != stdin){
+        fclose(file);
+    }
+}
+
 int main(int argc, char** argv){
     int lines = 10;
     int offset = 1;
     FILE* input = stdin;
     //Getting final count of lines if set -n
-    if(argc > 1){
-        if(strcmp(argv[offset], "-n")){
+    if(argc > 0){
+        if(strcmp(argv[offset], "-n") == 0){
             //Checking if -n has an argument
             if(argc < 3){
                 fprintf(stderr, "The value for -n must be set");
@@ -73,17 +89,32 @@ int main(int argc, char** argv){
             offset += 2; 
         }    
         //If source file given, then changing stdin to file
-        if(offset + 1 < argc){
+        if(offset < argc){
             input = fopen(argv[offset], "r");
         }   
     }
     
 
-    
-
-
-    if(input != stdin){
-        fclose(input);
+    circular_buffer* cb = cb_create(lines);
+    char* tmp_line = malloc(MAX_LINE_LENGTH * sizeof(char));
+    if(tmp_line == NULL){
+        cb_free(cb);
+        close_file_stream(input);
+        fprintf(stderr, "Failed to allocate memory");
+        return EXIT_FAILURE;
     }
+
+
+    while(fgets(tmp_line, MAX_LINE_LENGTH - 1, input) != NULL){
+        cb_put(cb, tmp_line);
+    }
+
+    for(int i = 0; i < lines; i++){
+        printf("%s", cb_get(cb));
+    }
+
+    free(tmp_line);
+    close_file_stream(input);
+    cb_free(cb);
     return EXIT_SUCCESS;
 }
